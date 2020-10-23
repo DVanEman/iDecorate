8c316f788fcfc8da6cdc964120f377ba17d95c9a
Victor Cesar Lopez <civ187@gmail.com>
2020-10-22 20:00:05 -0500

    second commit

---
+++ README.md
@@ -0,0 +1,50 @@
+
+# iDecorate
+
+![](https://img.shields.io/badge/Created%20by-Andrew%20van%20Eman-blue?style=for-the-badge)  ![](https://img.shields.io/badge/DeMario%20Williams-blue?style=for-the-badge)  ![](https://img.shields.io/badge/Jack%20Hoover-blue?style=for-the-badge)  ![](https://img.shields.io/badge/Ryanne%20killian-blue?style=for-the-badge)  ![](https://img.shields.io/badge/Victor%20Cesar%20Lopez-blue?style=for-the-badge)  
+
+
+![](https://img.shields.io/badge/npm%20package-Node-orange?style=flat-square&logo=npm) ![](https://img.shields.io/badge/npm%20package-Inquirer-cyan?style=flat-square&logo=npm) ![](https://img.shields.io/badge/npm%20package-MySQL-green?style=flat-square&logo=npm)  
+![](https://img.shields.io/badge/npm%20package-console.table-lightgrey?style=flat-square&logo=npm) ![](https://img.shields.io/badge/npm%20package-clear-blueviolet?style=flat-square&logo=npm) ![](https://img.shields.io/badge/npm%20package-ASCII_art_Logo-yellow?style=flat-square&logo=npm) 
+
+
+
+ ## Table of Contents:  
+
+[1. Description](#Description)  
+[2. Acceptance Criteria](#Acceptance-Criteria)  
+[3. Screen Shots](#Screen_Shots)  
+[4. Installation](#Installation)  
+[5. License Details](#License-Details)  
+[6. Submission](#Submission)   
+[7. Questions](#Questions)  
+
+## Description:  
+
+
+
+## Acceptance Criteria  
+
+
+
+## Screen Shots
+
+
+
+## Installation
+
+
+
+## License Details  
+
+
+## Submission  
+https://github.com/DVanEman/iDecorate
+
+## Questions:  
+ Here is a link to my github:  
+https://github.com/civ187  
+ Email me at:  
+civ187@gmail.com  
+for additional questions
+
--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
 
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
@@ -9,38 +9,41 @@ import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Nav from "./components/Nav";
+import { StoreProvider } from "./utils/GlobalState";
+
 import OrderHistory from "./pages/OrderHistory";
 
 const client = new ApolloClient({
   request: (operation) => {
-    const token = localStorage.getItem('id_token')
+    const token = localStorage.getItem("id_token");
     operation.setContext({
       headers: {
-        authorization: token ? `Bearer ${token}` : ''
-      }
-    })
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
   },
-  uri: '/graphql',
-})
+  uri: "/graphql",
+});
 
 function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <Nav />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/orderHistory" component={OrderHistory} />
-            <Route exact path="/products/:id" component={Detail} />
-            <Route component={NoMatch} />
-          </Switch>
+          <StoreProvider>
+            <Nav />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/orderHistory" component={OrderHistory} />
+              <Route exact path="/products/:id" component={Detail} />
+              <Route component={NoMatch} />
+            </Switch>
+          </StoreProvider>
         </div>
       </Router>
     </ApolloProvider>
-
   );
 }
 
